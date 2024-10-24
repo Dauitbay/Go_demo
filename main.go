ba0c8a004772f6431705f0b3a398dc504087b507
“firstname <dawitsarsenbaev@gmail.com>
2024-10-24 22:31:48 +0500

    Usage of the formatting in print

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ func main() {
 	const BMIPower = 2
 	userHeight := 1.8
 	userKilogram := 100.0
-	fmt.Print("__ Body Index Mass Calculator __ \n")
+	fmt.Println("__ Body Index Mass Calculator __ ")
 	fmt.Print("Enter your height in meter: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Enter your weight in kg: ")
 	fmt.Scan(&userKilogram)
 	BMI := userKilogram / math.Pow(userHeight, BMIPower)
+	fmt.Printf("Your BMI is: %.0f \n", BMI)
 	fmt.Print(BMI)
 
 }
