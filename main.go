59b067ae67da33f396436f25a0eecc7cd1e2f673
“firstname <dawitsarsenbaev@gmail.com>
2024-10-24 22:25:44 +0500

    Add print and scan to create interaction

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 	const BMIPower = 2
 	userHeight := 1.8
 	userKilogram := 100.0
+	fmt.Print("__ Body Index Mass Calculator __ \n")
+	fmt.Print("Enter your height in meter: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Enter your weight in kg: ")
+	fmt.Scan(&userKilogram)
 	BMI := userKilogram / math.Pow(userHeight, BMIPower)
 	fmt.Print(BMI)
 
