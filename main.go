20a1a59116c4ae91efc5f768250732281d7a4fda
“firstname <dawitsarsenbaev@gmail.com>
2024-10-27 21:09:43 +0500

    Add func getUserInput

--- main.go
+++ main.go
@@ -6,13 +6,8 @@ import (
 )
 
 func main() {
-	userHeight := 1.8
-	userKilogram := 100.0
 	fmt.Println("__ Body Index Mass Calculator __ ")
-	fmt.Print("Enter your height in meter: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Enter your weight in kg: ")
-	fmt.Scan(&userKilogram)
+	userKilogram, userHeight := getUserInput()
 	BMI := calculateBMI(userKilogram, userHeight)
 	outputResult(BMI)
 }
@@ -27,4 +22,14 @@ func calculateBMI(userKilogram float64, userHeight float64) float64 {
 	const BMIPower = 2
 	BMI := userKilogram / math.Pow(userHeight/100, BMIPower)
 	return BMI
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight float64
+	var userKilogram float64
+	fmt.Print("Enter your height in meter: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Enter your weight in kg: ")
+	fmt.Scan(&userKilogram)
+	return userKilogram, userHeight
 }
