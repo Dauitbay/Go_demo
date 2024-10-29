4671137f1d29a1f7115aaae5b26fab3a42f5a7eb
“firstname <dawitsarsenbaev@gmail.com>
2024-10-29 10:43:12 +0500

    Using operator break

--- main.go
+++ main.go
@@ -7,27 +7,32 @@ import (
 
 func main() {
 	fmt.Println("__ Body Index Mass Calculator __ ")
-	userKilogram, userHeight := getUserInput()
-	BMI := calculateBMI(userKilogram, userHeight)
-	outputResult(BMI)
-	isLean := BMI < 16
-	if isLean {
-		fmt.Println("You have strong deficit of the weight")
-	} else if BMI < 18.5 {
-		fmt.Println("You have deficit of the weight")
-	} else if BMI < 25 {
-		fmt.Println("You have normal weight")
-	} else if BMI < 30 {
-		fmt.Println("You are overweight")
-	} else {
-		fmt.Println("You have a degree of obesity")
+	for {
+		userKilogram, userHeight := getUserInput()
+		BMI := calculateBMI(userKilogram, userHeight)
+		outputResult(BMI)
+		isRepeatCalculation := checkRepeatCalculation()
+		if !isRepeatCalculation {
+			break
+		}
 	}
 }
-
 
 func outputResult(bmi float64) {
 	result := fmt.Sprintf("Your BMI is: %.0f \n", bmi)
 	fmt.Println(result)
+	isLean := bmi < 16
+	if isLean {
+		fmt.Println("You have strong deficit of the weight")
+	} else if bmi < 18.5 {
+		fmt.Println("You have deficit of the weight")
+	} else if bmi < 25 {
+		fmt.Println("You have normal weight")
+	} else if bmi < 30 {
+		fmt.Println("You are overweight")
+	} else {
+		fmt.Println("You have a degree of obesity")
+	}
 }
 
 func calculateBMI(userKilogram float64, userHeight float64) float64 {
@@ -44,4 +49,14 @@ func getUserInput() (float64, float64) {
 	fmt.Print("Enter your weight in kg: ")
 	fmt.Scan(&userKilogram)
 	return userKilogram, userHeight
+}
+
+func checkRepeatCalculation() bool {
+	var userChoice string
+	fmt.Print("Do you want to do caculation (y/n): ")
+	fmt.Scan(&userChoice)
+	if userChoice == "y" || userChoice == "Y" {
+		return true
+	}
+	return false
 }
